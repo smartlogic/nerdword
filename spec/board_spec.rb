f9d3fa33f3cc8c274b6ae8ebc9a14ecf3bdee3a0
Sam Goldman <samwgoldman@gmail.com>
2012-12-09 21:24:05 -0500

    Style change

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,47 +4,30 @@ require "position"
 
 describe Board do
   it "scores a move" do
-    values = {
-      "C" => 2,
-      "A" => 4,
-      "T" => 8
-    }
+    values = { ?C => 2, ?A => 4, ?T => 8 }
     board = Board.new(values)
-    score, tiles_used = board.play(Move.new("CAT", Position.new(0, 0), Direction::HORIZONTAL))
+
+    score, _ = board.play(Move.new("CAT", Position.new(0, 0), Direction::HORIZONTAL))
 
     score.should eq(14)
   end
 
   it "scores a multi-word move" do
-    values = {
-      "C" => 4,
-      "A" => 1,
-      "T" => 1,
-      "I" => 1,
-      "N" => 2
-    }
+    values = { ?C => 4, ?A => 1, ?T => 1, ?I => 1, ?N => 2 }
     board = Board.new(values)
 
-    # Move:
-    # CAT
-    #  TIN
-
     board.play(Move.new("CAT", Position.new(0, 0), Direction::HORIZONTAL))
-    score, tiles_used = board.play(Move.new("TIN", Position.new(1, 1), Direction::HORIZONTAL))
+    score, _ = board.play(Move.new("TIN", Position.new(1, 1), Direction::HORIZONTAL))
 
     score.should eq(8)
   end
 
   it "returns the tiles used to make the play" do
-    values = {
-      "C" => 2,
-      "A" => 4,
-      "T" => 8,
-      "S" => 1
-    }
+    values = { ?C => 1, ?A => 4, ?T => 8, ?S => 1 }
     board = Board.new(values)
-    score1, tiles_used1 = board.play(Move.new("CAT", Position.new(0, 0), Direction::HORIZONTAL))
-    score2, tiles_used2 = board.play(Move.new("CATS", Position.new(0, 0), Direction::HORIZONTAL))
+
+    _, tiles_used1 = board.play(Move.new("CAT", Position.new(0, 0), Direction::HORIZONTAL))
+    _, tiles_used2 = board.play(Move.new("CATS", Position.new(0, 0), Direction::HORIZONTAL))
 
     tiles_used1.should eq(%w{C A T})
     tiles_used2.should eq(%w{S})
