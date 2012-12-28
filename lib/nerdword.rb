df4fefdb2ea3217582aa71e3df97a3dd7f0d9d66
Eric Oestrich <eric@oestrich.org>
2012-12-28 17:46:32 -0500

    Create lib/nerdword.rb to require all necessary files

---
+++ lib/nerdword.rb
@@ -0,0 +1,7 @@
+require 'nerdword/board'
+require 'nerdword/direction'
+require 'nerdword/move'
+require 'nerdword/not_random'
+require 'nerdword/player'
+require 'nerdword/position'
+require 'nerdword/pouch'
