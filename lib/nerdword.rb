c30c74bb420a3430dcec5cb39b321414648656f5
Eric Oestrich <eric@oestrich.org>
2013-01-07 17:29:52 -0500

    Revert "Create lib/nerdword.rb to require all necessary files"
    This reverts commit df4fefdb2ea3217582aa71e3df97a3dd7f0d9d66.

--- lib/nerdword.rb
+++
@@ -1,7 +0,0 @@
-require 'nerdword/board'
-require 'nerdword/direction'
-require 'nerdword/move'
-require 'nerdword/not_random'
-require 'nerdword/player'
-require 'nerdword/position'
-require 'nerdword/pouch'
